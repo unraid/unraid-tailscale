5fe304935b442f0604c9a1a59a28b6b44eb33431
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2025-09-25 21:05:36 +0000

    chore: remove old migration task
    Signed-off-by: Derek Kaser <11674153+dkaser@users.noreply.github.com>

--- src/usr/local/emhttp/plugins/tailscale/update-settings.sh
+++ src/usr/local/emhttp/plugins/tailscale/update-settings.sh
@@ -5,12 +5,6 @@
 TS_PLUGIN_CONFIG=/boot/config/plugins/tailscale/tailscale.cfg
 TS_PLUGIN_ROOT=/usr/local/emhttp/plugins/tailscale
 
-# Cleanup Taildrop emulation from pre-1.42.0
-if [ ! -s /etc/config/uLinux.conf ] && [ -f /etc/config/uLinux.conf ]; then
-    log "Cleaning up Taildrop emulation"
-    rm /etc/config/uLinux.conf
-fi
-
 if [ -f $TS_PLUGIN_CONFIG ]; then
     source $TS_PLUGIN_CONFIG
 fi
