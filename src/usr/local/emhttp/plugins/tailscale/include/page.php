a2658504f423defd8272439b737fe6f69ef16095
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2025-10-04 01:54:58 +0000

    fix: end buffer if exception occurs in page
    Signed-off-by: Derek Kaser <11674153+dkaser@users.noreply.github.com>

--- src/usr/local/emhttp/plugins/tailscale/include/page.php
+++ src/usr/local/emhttp/plugins/tailscale/include/page.php
@@ -46,8 +46,13 @@ function includePage(string $filename, array $params = array()): string
 
     if (is_file($filename)) {
         ob_start();
-        include $filename;
-        return ob_get_clean() ?: "";
+        try {
+            include $filename;
+            return ob_get_clean() ?: "";
+        } catch (\Throwable $e) {
+            ob_end_clean();
+            throw $e;
+        }
     }
     return "";
 }
