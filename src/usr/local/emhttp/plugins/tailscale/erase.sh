cefa4396c3171ba49d28e5e4721a8872ec661890
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2026-06-30 21:24:30 -0400

    fix: old references in erase script (#109)
    Signed-off-by: Derek Kaser <11674153+dkaser@users.noreply.github.com>

--- src/usr/local/emhttp/plugins/tailscale/erase.sh
+++ src/usr/local/emhttp/plugins/tailscale/erase.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. /usr/local/emhttp/plugins/tailscale/log.sh
+. /usr/local/php/unraid-tailscale-utils/log.sh
 
 log "Stopping Tailscale"
 /etc/rc.d/rc.tailscale stop
@@ -10,4 +10,6 @@ rm -f /boot/config/plugins/tailscale/tailscale.cfg
 rm -rf  /boot/config/plugins/tailscale/state/
 
 log "Restarting Tailscale"
-echo "sleep 5 ; /usr/local/emhttp/plugins/tailscale/update-settings.sh" | at now 2>/dev/null
+/etc/rc.d/rc.tailscale start
+
+sleep 10
